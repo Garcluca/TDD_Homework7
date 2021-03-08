19f7cdf35f8bdf6713e85164a8a3d022cf3fe156
Lucas <lg6815@gmail.com>
2021-03-08 02:06:00 -0800

    Wrote first test, tests that fizzbuzz works correctly

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,20 +1,4 @@
 def code():
     #i = 1
     result = ""
-    for i in range(101):
-        if i == 0 :
-            continue
-        if i % 15==0 and i !=0:
-            result+="Fizzbuzz"
-            print("FizzBuzz")
-        elif i % 3==0:
-            result+= "Fizz"
-            print("Fizz")
-        elif i % 5==0:
-            result+= "Buzz"
-            print("Buzz")
-        else:
-            result+= str(i)
-            print(i)
-    print(result)
     return result 
